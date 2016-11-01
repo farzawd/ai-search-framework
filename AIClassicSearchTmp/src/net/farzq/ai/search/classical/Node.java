30d2dfbbed5d888b174e34307db07349835567ab
Farzad <Farzad@farzawd-pc>
2016-11-01 19:16:04 +0330

    Added some basic code.

--- AIClassicSearchTmp/src/net/farzq/ai/search/classical/Node.java
+++ AIClassicSearchTmp/src/net/farzq/ai/search/classical/Node.java
@@ -1,6 +1,23 @@
 package net.farzq.ai.search.classical;
 
-public class Node
+public class Node<T extends IState>
 {
-	private Node parent;
+	private Node<T> parent;
+	private T state;
+	
+	public Node(T state, Node<T> parent)
+	{
+		this.state = state;
+		this.parent = parent;
+	}
+
+	public Node<T> getParent()
+	{
+		return parent;
+	}
+
+	public T getState()
+	{
+		return state;
+	}
 }
